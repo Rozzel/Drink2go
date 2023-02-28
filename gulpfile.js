e11cb38ab15280321aab608c1f56812af9013a11
Vladislav Golik <vladislav.golik@gmail.com>
2023-02-28 11:35:09 +0300

    Настроил gulp-replacel, сброс кэша CSS и JS

--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,26 @@ import csso from 'postcss-csso';
 import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
+import replace from "gulp-replace";
 import { stacksvg } from "gulp-stacksvg";
 import { deleteAsync } from 'del';
 import browser from 'browser-sync';
 import bemlinter from 'gulp-html-bemlinter';
 import { htmlValidator } from "gulp-w3c-html-validator";
 
+let date = new Date();
+
 const sass = gulpSass(dartSass);
 let isDevelopment = true;
 
 export function processMarkup () {
   return gulp.src('source/*.html')
+    .pipe(
+      replace(
+        "?v=cache",
+        `?v=${date.getFullYear()}${date.getMonth()}${date.getDate()}${date.getHours()}${date.getMinutes()}`
+      )
+    )
     .pipe(gulp.dest('build'));
 }
 
